4d65c87a2d8d8b153d0b96b91eed72ce838bd1a5
Paul Marks <paul@pmarks.net>
2023-05-22 17:44:18 -0400

    Add click-to-toggle feature.

--- src/background.js
+++ src/background.js
@@ -1,5 +1,52 @@
-chrome.offscreen.createDocument({
-  url: chrome.runtime.getURL('offscreen.html'),
-  reasons: ['AUDIO_PLAYBACK'],
-  justification: 'Plays almost-silence continuously to keep the sound card awake',
+let isSoundOn = null;
+
+const init = (async () => {
+  const data = await chrome.storage.local.get(['isSoundOn']);
+  if (data.isSoundOn !== undefined) {
+    isSoundOn = data.isSoundOn;
+  } else {
+    isSoundOn = true;  // default state
+  }
+  apply();
+})();
+
+chrome.action.onClicked.addListener(async (tab) => {
+  await init;
+  isSoundOn = !isSoundOn;
+  chrome.storage.local.set({ isSoundOn });
+  apply();
 });
+
+async function apply() {
+  if (!(busy && isSoundOn == await busy)) {
+    busy = setupOffscreenDocument(isSoundOn);
+    await busy;
+    busy = null;
+  }
+}
+
+let busy = null;
+async function setupOffscreenDocument(newExists) {
+  const url = chrome.runtime.getURL('offscreen.html');
+  let oldExists = false;
+  for (const client of await clients.matchAll({includeUncontrolled: true})) {
+    if (client.url === url) {
+      oldExists = true;
+      break;
+    }
+  }
+  if (oldExists != newExists) {
+    if (newExists) {
+      chrome.offscreen.createDocument({
+        url: url,
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'Plays almost-silence continuously to keep the sound card awake',
+      });
+      chrome.action.setIcon({ path: 'icon128.png' });
+    } else {
+      chrome.offscreen.closeDocument();
+      chrome.action.setIcon({ path: 'icon128-gray.png' });
+    }
+  }
+  return newExists;
+}
